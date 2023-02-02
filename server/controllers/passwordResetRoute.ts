ef0c1bf0fdda25a8529beb5d7ccbff153634e1a9
Ike <isaac.sarfo-anokye@amalitech.org>
2023-02-02 11:42:14 -1200

    login Route created

---
+++ server/controllers/passwordResetRoute.ts
@@ -0,0 +1,57 @@
+const express = require("express");
+const client = require("../db");
+const jwt = require("jsonwebtoken");
+const passwordResetRoute = express.Router();
+var nodemailer = require("nodemailer");
+
+//setting up nodemailer with transport
+var transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "nodetestmail123@gmail.com",
+    pass: "socgztlrtowjuxrl",
+  },
+});
+
+const JWT_SECRET = "i love books";
+passwordResetRoute.post("/reset-password", async (req: any, res: any) => {
+  const { email } = req.body;
+  //check is email exist in the database
+  try {
+    const emailExist = await client.query(
+      "SELECT * FROM users WHERE email=$1",
+      [email]
+    );
+    if (emailExist.rowCount != 0) {
+      const secret = JWT_SECRET + emailExist.rows[0].password;
+      const payload = {
+        email: emailExist.rows[0].email,
+        id: emailExist.rows[0].id,
+      };
+      const token = jwt.sign(payload, secret, { expiresIn: "1m" });
+      const link = `http://localhost:3000/reset-password/${payload.id}/${token}`;
+
+      const mailOptions = {
+        //nodemailer options for sending mail to the user
+        from: "nodetestmail123@gmail.com", // sender address
+        to: `${email}`, // list of receivers
+        subject: "RESET PASSWORD", // Subject line
+        html: `<a href=${link}>reset</a>`, // plain text body
+      };
+      transporter.sendMail(mailOptions, function (err: any, info: any) {
+        //sending mail using the options provide above
+        if (err) console.log(err);
+        else console.log(info);
+        res.json({ message: "Reset link has been sent to your mail" });
+      });
+    } else {
+      console.log("user not registered");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = passwordResetRoute;
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
 const express = require('express');
 const client=require('./db')
 // const registerRoute=require('./controllers/registerRoute');
-// const loginRoute=require('./controllers/loginRoute');
+const loginRoute=require('./controllers/loginRoute');
 // const uploadRoute=require('./controllers/uploadRoute');
 const getfilesRoute=require('./controllers/getfilesRoute');
 const filedownloadRoute=require('./controllers/filedownloadRoute');
-// const passwordResetRoute=require('./controllers/passwordResetRoute');
+const passwordResetRoute=require('./controllers/passwordResetRoute');
 // const passwordRRoute=require('./controllers/passwordRRoute');
 // const fileupload = require("express-fileupload");
 // const serveIndex=require('serve-index');
@@ -18,11 +18,11 @@ app.use(express.json())
 // app.use(fileupload())
 // app.use('/ftp',express.static('./uploads'),serveIndex('./uploads',{icons:true}))
 // app.use('/',registerRoute);
-// app.use('/',loginRoute);
+app.use('/',loginRoute);
 // app.use('/',uploadRoute);
 app.use('/',getfilesRoute);
 app.use('/',filedownloadRoute);
-// app.use('/',passwordResetRoute);
+app.use('/',passwordResetRoute);
 // app.use('/',passwordRRoute);
 
 
